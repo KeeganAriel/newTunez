bbfb62dc64bc81ecd8a4b955d063222056f8dc72
KeeganAriel <keegan.teinowitz@gmail.com>
2016-05-19 13:29:45 -0700

    add ids/ try to get JSON
    btn click not console logging

--- js/app.js
+++ js/app.js
@@ -2,3 +2,27 @@
 // use user name to get query
 // display artest names
 // last fm to imvdb
+
+
+$(document).ready(function () {
+	// $('#sumbit').click(function(event) {
+	// 	event.preventDefault();
+	// 	var search = $('#imput').val();
+	// 		console.log("pizza");
+
+		// $.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user='+ search +'&api_key='+ key +'&format=json', 
+		// 	{user : search,
+		// 	period : "7day",
+		// 	limit : 10,
+		// 	key: "9f2a9dfab67cd0feeb756b3292fb2f4e",
+		// 	}, function(data) {
+		// 	console.log(data);
+		// });
+
+	// });
+	var userImput = $('#imput').val();
+	$('#sumbit').click(function() {
+		console.log(userImput);
+	});
+
+});
