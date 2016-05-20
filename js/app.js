eb505eb31ec2b910df8340b772c5f724998be881
KeeganAriel <keegan.teinowitz@gmail.com>
2016-05-19 19:59:07 -0700

    add fonts and css formatting for template

--- js/app.js
+++ js/app.js
@@ -5,24 +5,30 @@
 
 
 $(document).ready(function () {
-	// $('#sumbit').click(function(event) {
-	// 	event.preventDefault();
-	// 	var search = $('#input').val();
-	// 		console.log("pizza");
+	$('#submit').click(function(event) {
+		event.preventDefault();
+		var userInput = $('#username').val();
+			console.log(userInput);
 
-		// $.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user='+ search +'&api_key='+ key +'&format=json', 
-		// 	{user : search,
-		// 	period : "7day",
-		// 	limit : 10,
-		// 	key: "9f2a9dfab67cd0feeb756b3292fb2f4e",
-		// 	}, function(data) {
-		// 	console.log(data);
-		// });
+		$.getJSON('http://ws.audioscrobbler.com/2.0/', 
+			{method : 'user.gettoptracks',
+			user : userInput,
+			period : "7day",
+			limit : 10,
+			api_key: "9f2a9dfab67cd0feeb756b3292fb2f4e",
+			format : 'json',
+			}, function(data) {
+			console.log(data);
+			var template = $('#resultTemplate').html();
+			var render = Handlebars.compile(template);
+			data.toptracks.track.forEach(function(track) {
+				console.log(track);
+				$('.results').append(render(track));
+			});
+		});
 
-	// });
-	var userImput = $('#input').val();
-	$('#sumbit').submit(function() {
-		console.log(userImput);
 	});
+
+
 
 });
